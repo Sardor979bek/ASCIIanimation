316ef560a25de1a427f9dc51b0325888dfb73b83
Sardor979bek <37942959+Sardor979bek@users.noreply.github.com>
2022-05-05 10:48:22 -0500

    Style change

--- ascii.js
+++ ascii.js
@@ -2,18 +2,17 @@ var ANIMATIONS = { Bike: BIKE, Exercise: EXERCISE, Dive: DIVE, Juggler: JUGGLER,
 
 var mytextarea = document.getElementById('mytextarea');
 var startBtn = document.getElementById('start');
-var stopBtn = document.getElementById('stop')
+var stopBtn = document.getElementById('stop');
+
 
 startBtn.onclick = function () { start() };
 stopBtn.onclick = function () { stop() };
 
-var lists;
+var lists_animation;
 var current = 0;
 var end;
 var speed = 250;
-var custom = false;
 
-timer = null;
 
 document.getElementById('speed').onchange = function () {
     if (this.checked) speed = 50;
@@ -23,9 +22,11 @@ document.getElementById('speed').onchange = function () {
 }
 document.getElementById('size').onchange = function () {
     mytextarea.style.fontSize = this.value + "pt";
-
+    document.getElementsByClassName('mybox')[0].style.width = '100%';
 }
 
+
+var custom = false;
 document.getElementById('animations').onchange = function () {
     stop();
     mytextarea.value = "";
@@ -33,7 +34,7 @@ document.getElementById('animations').onchange = function () {
         custom = true;
     } else {
         console.log(this.value);
-        lists = ANIMATIONS[this.value].split('=====');
+        lists_animation = ANIMATIONS[this.value].split('=====');
         custom = false;
     }
     if (!custom) config();
@@ -42,22 +43,22 @@ document.getElementById('animations').onchange = function () {
 
 function config() {
     current = 0;
-    end = lists.length;
+    end = lists_animation.length;
     if (end > 0)
         set();
     else stop();
 }
 
 function set() {
-    mytextarea.value = lists[current];
+    mytextarea.value = lists_animation[current];
     if (current + 1 > end - 1) current = 0;
     else current++;
 }
 
-
+timer = null;
 function start() {
     if (custom) {
-        lists = mytextarea.value.split('=====');
+        lists_animation = mytextarea.value.split('=====');
         config();
         custom = false;
 
